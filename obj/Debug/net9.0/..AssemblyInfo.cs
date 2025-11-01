15c44ea859eb60db1a0da4b2098a389664f83a24
Sayi <redwaw919@gmail.com>
2025-11-01 22:56:05 +0800

    Navbar and footer

--- obj/Debug/net9.0/..AssemblyInfo.cs
+++ obj/Debug/net9.0/..AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute(".")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5339d62e4d6c9487e5d15b81d5c5c141b7f03d12")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9df6b948b5cc3950e258cadf8ed27149782dfef")]
 [assembly: System.Reflection.AssemblyProductAttribute(".")]
 [assembly: System.Reflection.AssemblyTitleAttribute(".")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
