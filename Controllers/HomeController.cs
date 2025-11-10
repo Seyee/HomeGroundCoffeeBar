0bbd7f7ebcdde43cb193e167a30c6c1f312f2441
Sayi <redwaw919@gmail.com>
2025-11-10 16:47:05 +0800

    SIGNUP SIGNIN DONE

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,6 +28,16 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult Signin()
+    {
+        return View();
+    }
+
+    public IActionResult Signup()
+    {
+        return View();
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
