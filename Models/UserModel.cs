89c0f2c892f29ef12371059fe54114193ff644ac
Kyle <kylegonzales585@gmail.com>
2026-03-14 20:24:51 +0800

    changes

--- Models/UserModel.cs
+++ Models/UserModel.cs
@@ -4,11 +4,11 @@ namespace Models
     {
         public int Id { get; set; }        
         public string Name {get; set;}
-        public string Phone {get; set;}
+        public string? Phone { get; set; }
         public string Password {get; set;}
         public string ProfilePic {get; set;}
         public string GoogleId { get; set; }
-        public DateTime? CreatedAt { get; set; }
+        public DateTime? CreatedAt { get; set; }                                              
         public string Role {get; set;}
         public int Points { get; set; } = 0; 
     }
